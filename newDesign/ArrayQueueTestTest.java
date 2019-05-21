a70dca484d60a5a010e304646cd6b8d905f73c12
Fredrik <fredrikkortetjarvi@protonmail.com>
2019-05-21 12:56:25 +0200

    fe

--- newDesign/ArrayQueueTestTest.java
+++ newDesign/ArrayQueueTestTest.java
@@ -1,7 +1,6 @@
-import static org.junit.jupiter.api.Assertions.*;
-
+import static org.junit.Assert.*;
 import org.junit.Before;
-import org.junit.jupiter.api.Test;
+import org.junit.Test;
 
 public class ArrayQueueTestTest
 {
